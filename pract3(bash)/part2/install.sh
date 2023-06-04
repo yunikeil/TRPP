ceb7f19157edcfad70e38194dfc32eace0229331
Ilia Yunakov <76445770+yunikeil@users.noreply.github.com>
2023-06-05 00:37:38 +0300

    -

--- pract3(bash)/part2/install.sh
+++ pract3(bash)/part2/install.sh
@@ -3,10 +3,13 @@
 mkdir ProjectDjango
 cd ProjectDjango
 
+python3 -m venv Venv
+source Venv/bin/activate
+
 wget https://www.dropbox.com/s/ija7ax3sj6ysb0p/blocknote-master.tar.gz
 tar -xvf blocknote-master.tar.gz
 
-wget  https://github.com/yunikeil/TRPP/pract3(bash)/part2/new_requirements.py
+wget "https://github.com/yunikeil/TRPP/blob/main/pract3(bash)/part2/new_requirements.py"
 
 python3 new_requirements.py
 pip install -r requirements.txt
